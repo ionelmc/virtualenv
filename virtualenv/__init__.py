6ac4764790526f435ffc6337145439d710dd455f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-12 20:53:28 +0200

    Add a backwards compatible create_environment.

--- virtualenv/__init__.py
+++ virtualenv/__init__.py
@@ -7,6 +7,23 @@ from virtualenv.__about__ import (
 from virtualenv.core import create
 
 
+def create_environment(
+    home_dir,
+    site_packages=False, clear=False,
+    unzip_setuptools=False,
+    prompt=None, search_dirs=None, never_download=False,
+    no_setuptools=False, no_pip=False, symlink=True
+):
+    create(
+        home_dir,
+        system_site_packages=site_packages,
+        clear=clear,
+        prompt=prompt or "",
+        extra_search_dirs=search_dirs,
+        setuptools=not no_setuptools,
+        pip=not no_pip
+    )
+
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
