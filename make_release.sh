7ad3e38a5e03085b19ce2d78a34cc8b45d36ea11
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-12-22 02:02:06 +0200

    Also clean here.

--- make_release.sh
+++ make_release.sh
@@ -6,5 +6,6 @@ rm -rf dist build *.egg-info
 python setup.py clean --all sdist bdist_wheel
 twine upload dist/*
 
+rm -rf dist build *.egg-info
 DIST=virtualenv python setup.py clean --all sdist bdist_wheel
 binstar upload dist/*
